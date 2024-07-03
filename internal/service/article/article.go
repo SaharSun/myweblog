48f63ac716a429db1b115c6560a983c923a7674d
sahar <sahar8013@gmail.com>
2024-07-03 21:38:43 +0330

    test1

--- internal/service/article/article.go
+++ internal/service/article/article.go
@@ -1,24 +1,35 @@
 package article
 
 import (
+	"errors"
 	"myweblog/internal/repository/article"
 
 	"github.com/SaharSun/myweblog/internal/repository/article"
+	// "github.com/rs/zerolog/log"
 
 	entityArticle "github.com/SaharSun/myweblog/internal/entity/article"
 )
 
 type Article struct {
-	articleRepository article.Repository 
+	articleRepository article.Repository
+	// logger            loggerInfra.Logger
 }
 
-func NewArticle(article article.Repository)*Article {
+func NewArticle(articleRepository article.Repository) *Article {
 	return &Article{
 		articleRepository: articleRepository,
+		// logger:            logger,
 	}
 }
 
 
-func (a Article)Create(artEnt *entityArticle.Article)error {
-	a.articleRepository.Creat(artEnt)
+func (a Article) Create(artEnt *entityArticle.Article) error {
+	// if err := a.articleRepository.Creat(artEnt); err != nil {
+	// 	if errors.Is(err, article.ErrArticleAlreadyExist) {
+	// 		return err
+	// 	}
+	// 	log.Error().Err(err).Msg("error in creating article")
+	// 	return err
+	// }
+	// return nil
 }
