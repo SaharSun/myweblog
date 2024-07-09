001355a24247dd06bc071d1cd589ddbc325b702b
sahar <sahar8013@gmail.com>
2024-07-09 20:53:26 +0330

    add logg files

--- internal/service/article/article.go
+++ internal/service/article/article.go
@@ -1,9 +1,9 @@
 package article
 
 import (
-	// "errors"
+	"errors"
 
-	"https://github.com/SaharSun/myweblog/internal/repository/article"
+	"github.com/SaharSun/myweblog/internal/repository/article"
 	// "github.com/rs/zerolog/log"
 
 	entityArticle "github.com/SaharSun/myweblog/internal/entity/article"
@@ -22,12 +22,13 @@ func NewArticle(articleRepository article.Repository) *Article {
 }
 
 func (a Article) Create(artEnt *entityArticle.Article) error {
-	// if err := a.articleRepository.Creat(artEnt); err != nil {
-	// 	if errors.Is(err, article.ErrArticleAlreadyExist) {
-	// 		return err
-	// 	}
-	// 	log.Error().Err(err).Msg("error in creating article")
-	// 	return err
-	// }
-	return nil
+	id, err := a.articleRepository.Create(artEnt)
+	if err != nil {
+		if errors.Is(err, article.ErrArticleAlreadyExist) {
+			return id, err
+		}
+		a.logger.Error(err.Error())
+		return id, err
+	}
+	return id, nil
 }
